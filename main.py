d2c1e7af1f738533168535195cd73d3062b55420
Alexander Dietrich <102086038+dietrichalex@users.noreply.github.com>
2024-11-01 13:31:17 +0100

    read data

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import csv
+import pandas as pd
 import numpy as np
 
+def get_data():
+   with open('data/Scouting_Reports_FCA.csv', encoding="utf8") as csvfile:
+       csv_reader = csv.reader(csvfile, delimiter=';')
+       for row in csv_reader:
+           print(row)
+
+
+def main():
+    get_data()
+
+if __name__ == '__main__':
+    main()
