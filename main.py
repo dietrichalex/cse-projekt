91e4ababe60db145aa71a172e28d86087f3b1050
Alex Dietrich <alexander.dietrich@uni-ulm.de>
2024-11-07 11:15:59 +0100

    update data reading + remove unnecessary columns

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import csv
 import pandas as pd
 import numpy as np
 
 def get_data():
-   with open('data/Scouting_Reports_FCA.csv', encoding="utf8") as csvfile:
-       csv_reader = csv.reader(csvfile, delimiter=';')
-       for row in csv_reader:
-           print(row)
+    data = np.array(pd.read_csv('data/Scouting_Reports_FCA.csv', encoding="utf8", delimiter=';'))
+    return data
+
+def filter_data(data):
+    data = data[:,:-5] #remove unnecessary columns
+    #TODO: do more filtering
+    return data
 
 
 def main():
-    get_data()
+    data = get_data()
+    data = filter_data(data)
 
 if __name__ == '__main__':
     main()
