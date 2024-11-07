2ca0f009ddbab907d7438d406768d7e052b7fdb5
Alex Dietrich <alexander.dietrich@uni-ulm.de>
2024-11-07 11:29:36 +0100

    only use pandas + first useful filter

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pandas as pd
-import numpy as np
 
 def get_data():
-    data = np.array(pd.read_csv('data/Scouting_Reports_FCA.csv', encoding="utf8", delimiter=';'))
+    data = pd.read_csv('data/Scouting_Reports_FCA.csv', encoding="utf8", delimiter=';')
+    data.columns = data.columns.str.replace('Column1.', '', regex=False)
     return data
 
 def filter_data(data):
-    data = data[:,:-5] #remove unnecessary columns
-    #TODO: do more filtering
+    # remove unnecessary columns
+    data = data.iloc[:,:-5]
+
+    #filter players with less than 10 matches
+    minimum_match_amount = 10
+    id_counts = data['PlayerId'].value_counts()
+    data = data[data['PlayerId'].isin(id_counts[id_counts >= minimum_match_amount].index)]
     return data
 
 
