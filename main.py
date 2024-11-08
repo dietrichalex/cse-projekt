7d82be823b263ca042de99642694a5ef2154b125
Alexander Dietrich <102086038+dietrichalex@users.noreply.github.com>
2024-11-08 19:06:44 +0100

    player occurence vs percentage plot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 def get_data():
@@ -16,10 +17,23 @@ def filter_data(data):
     data = data[data['PlayerId'].isin(id_counts[id_counts >= minimum_match_amount].index)]
     return data
 
+def plot_player_occurrence_percentage(data):
+    count = data['PlayerId'].value_counts()
+    count.plot(kind='line', marker='o')
+    plt.xlabel('Amount of Data in Percentage')
+    plt.ylabel('Occurrences')
+    plt.title('Occurrences per Percentage')
+    xticks = range(0, len(count), max(1, len(count) // 10))
+    xtick_labels = [f"{(i / len(count)) * 100:.0f}%" for i in xticks]
+    plt.xticks(xticks, labels=xtick_labels)
+    plt.grid(True)
+    plt.show()
 
 def main():
     data = get_data()
+    plot_player_occurrence_percentage(data)
     data = filter_data(data)
+
 
 if __name__ == '__main__':
     main()
