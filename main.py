fd57a4ce992d1969285f66227ecd18134554b28c
Alexander Dietrich <102086038+dietrichalex@users.noreply.github.com>
2024-11-16 14:02:46 +0100

    filtered reports

--- main.py
+++ main.py
@@ -22,11 +22,27 @@ def filter_data(data):
     minimum_match_amount = 5
     id_counts = data['PlayerId'].value_counts()
     data = data[data['PlayerId'].isin(id_counts[id_counts >= minimum_match_amount].index)]
+
     return data
+
+def print_word_count(data, min_words, max_words, filename):
+    filter_words = data['word_count'] = data['Comment'].apply(
+        lambda x: len(str(x).split()) if isinstance(x, str) else 0)
+    filtered_data = data[data['word_count'] > min_words]
+    filtered_data = filtered_data[filtered_data['word_count'] <= max_words]
+    # Group by 'ScoutID' and take up to 3 entries per group
+    filtered_data = filtered_data.groupby('ScoutId').head(3)
+
+    # Save to a text file
+    with open(filename, 'w', encoding='utf-8') as f:
+        for _, row in filtered_data.iterrows():
+            f.write(f"-------------------------------------------------------------------------------------------------------------------------------------------------------\n")
+            f.write(f"ScoutID: {row['ScoutId']} \nComment: {row['Comment']} \nWord Count: {row['word_count']}\n\n")
 
 def main():
     data = get_data()
     data = filter_data(data)
+    print_word_count(data,30, 90 ,"filtered_words.txt")
 
 
 if __name__ == '__main__':
