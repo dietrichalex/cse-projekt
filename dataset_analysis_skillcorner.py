03c2a1e0656d54334e313ce0953ca74279c4844b
TimGrundstein <tim.grundstein@live.de>
2025-05-31 10:11:32 +0200

    changed dataset_analysis_skillcorner.py working - added primitive timeline function - added primitive analyse_passing

--- dataset_analysis_skillcorner.py
+++ dataset_analysis_skillcorner.py
@@ -46,12 +46,75 @@ def plot_heatmap(data, player_id):
     plt.show()
 
 
+def analyse_passing(data, player_id):
+    player_data = data[data['player_id'] == player_id]
+
+    # calculate the avg possession time
+    player_posstime_data = player_data[player_data['event_type_id'] == 8]
+    nof_poss = len(player_posstime_data)
+    sum_duration = player_posstime_data['duration'].sum()
+    if nof_poss > 0:
+        avg_poss = sum_duration / nof_poss
+    else:
+        avg_poss = 0
+
+    print(f"{player_id} has a average Possession time of {avg_poss:.2f}s with {nof_poss} Possessions")
+
+    # Count total and successful passes
+    player_passes_data = player_data[player_data['end_type_id'] == 1]
+    total_passes = len(player_passes_data)
+    successful_passes = len(player_passes_data[player_passes_data['pass_outcome'] == 'successful'])
+
+    # Calculate accuracy
+    if total_passes > 0:
+        pass_accuracy = (successful_passes / total_passes) * 100
+    else:
+        pass_accuracy = 0.0
+
+    print(f"{player_id} has a Pass Accuracy of {pass_accuracy:.2f}% with {total_passes} Passes")
+    # Only successful pass are included in this column
+    pass_type = player_passes_data['pass_range'].value_counts()
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=pass_type.values, y=pass_type.index, hue=pass_type.index, palette="Blues_d", legend=False)
+    plt.xlabel("Number of Passes")
+    plt.ylabel("Pass Type")
+    plt.title("Pass Type Value Counts")
+    plt.grid(axis='x', linestyle='--', alpha=0.6)
+    plt.tight_layout()
+    plt.show()
+
+
+def timeline(data, player_id):
+    player_data = data[data['player_id'] == player_id]
+
+    # Sort by time
+    player_data = player_data.sort_values(by=["period","time_start"])
+
+    # Plot
+    plt.figure(figsize=(12, 4))
+
+    plt.scatter(player_data["time_start"], player_data["event_type"], c='blue', alpha=0.6)
+
+    plt.title(f"Event Timeline for {player_id}")
+    plt.xlabel("Time")
+    plt.ylabel("Event Type")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+
 def main():
+    # get data
     data = get_data()
     #print(data)
-
     plot_nof_actions(data, 20)
-    plot_heatmap(data, 24470)
+    # select player to analyse
+    curr_player = 11757
+    # analyse player
+    plot_heatmap(data, curr_player)
+    analyse_passing(data, curr_player)
+    timeline(data, curr_player)
 
 if __name__ == '__main__':
     main()
